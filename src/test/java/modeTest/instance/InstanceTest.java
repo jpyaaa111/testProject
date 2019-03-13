c91583d221e4b074e0490cd44939866ad38d9d86
jpyaaa111 <573311280@qq.com>
2019-03-13 10:25:57 +0800

    修改了单例模式的注释, 修改了原来懒汉 与 饿汉的理解错误

--- src/test/java/modeTest/instance/InstanceTest.java
+++ src/test/java/modeTest/instance/InstanceTest.java
@@ -1,18 +1,18 @@
 package modeTest.instance;
 
 /**
- * 懒汉模式单例, 最简单的单例模式
+ * 饿汉模式单例, 最简单的单例模式
  * 优点 : 简单方便, 速度快
  * 缺点 : 初始化加载类时就加载的对象, 占用内存
  * @author : JinPeiyao
  * @version : V1.0
  * @Title : InstanceTest
- * @Description : 懒汉单例
+ * @Description : 饿汉单例
  * @date : 2019-1-31 15:03:37
  */
 public class InstanceTest {
 
-    // 懒汉, 加载类时, 直接新建单例
+    // 饿汉, 加载类时, 直接新建单例
     private static InstanceTest instance = new InstanceTest();
 
     // 私有化构造参数
