a9a0b40de020a6c48b6349298711371aa4b08ca9
jpyaaa111 <573311280@qq.com>
2020-10-16 16:15:17 +0800

    去除了业务的工具方法
    Signed-off-by: jpyaaa111 <573311280@qq.com>

--- src/main/java/utils/MyStringUtil.java
+++ src/main/java/utils/MyStringUtil.java
@@ -10,30 +10,5 @@ package utils;
  */
 public class MyStringUtil {
 
-    /**
-     * 在分页查询条件中, 去除开头and拼接, and的格式固定, 必须为' AND '才可使用本方法
-     * @Title filterRmAnd
-     * @Description
-     * @author JinPeiyao
-     * @param filter String, 拼接完成的分页查询条件字符串
-     * @date 2020/10/10 10:45
-     * @return java.lang.String 去除开头and后的分页查询参数
-     */
-    public static String filterRmAnd(String filter) {
-        return filterRmAnd(filter, " AND ");
-    }
 
-    /**
-     * 在分页查询条件中, 去除开头and拼接, and拼接需要传入, 防止拼接的and格式不同, 导致无法正确截取
-     * @Title filterRmAnd
-     * @Description
-     * @author JinPeiyao
-     * @param filter String, 拼接完成的分页查询条件字符串
-     * @param appendAnd String, 拼接的and字符串, 为了防止格式不统一
-     * @date 2020/10/10 10:36
-     * @return java.lang.String 去除开头and后的分页查询参数
-     */
-    public static String filterRmAnd(String filter, String appendAnd) {
-        return filter.indexOf(appendAnd) == 0 ? filter.substring(filter.indexOf(appendAnd) + appendAnd.length()) : filter;
-    }
 }
